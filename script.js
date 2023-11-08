849ed53be43f4def7e8fa5adfada3484c67c2d7e
AlbinAndersson1337 <118989634+AlbinAndersson1337@users.noreply.github.com>
2023-11-08 18:49:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,89 @@
 let antalPlankor = 0;
 let zombieClickCounter = 0;
+let antalPengar = 0;
+let antalMaterial = 0;
+let antalVapendelar = 0;
 const reqClicks = 10;
 const clicksTKill = document.querySelector(".clicks-to-kill");
 const zombieImage = document.querySelector(".left-section");
 
-window.addEventListener("load", function () {
-  zombieImage.addEventListener("click", function () {
-    zombieClickCounter++;
-    clicksTKill.textContent -= 1;
+function genereraSlumpadeResurser(zombieTyp) {
+  switch (zombieTyp) {
+    case 1:
+      return {
+        plankor: Math.floor(Math.random() * 3) + 1,
+        pengar: 100,
+        material: Math.floor(Math.random() * 3) + 3,
+        vapendelar: Math.floor(Math.random() * 3) + 1,
+      };
+    case 2:
+      return {
+        plankor: Math.floor(Math.random() * 4) + 3,
+        pengar: 300,
+        material: Math.floor(Math.random() * 7) + 9,
+        vapendelar: Math.floor(Math.random() * 4) + 3,
+      };
+    case 3:
+      return {
+        plankor: 9,
+        pengar: 500,
+        material: Math.floor(Math.random() * 6) + 15,
+        vapendelar: Math.floor(Math.random() * 3) + 9,
+      };
+    default:
+      return {
+        plankor: 0,
+        pengar: 0,
+        material: 0,
+        vapendelar: 0,
+      };
+  }
+}
 
-    if (clicksTKill.textContent == 0) {
-      clicksTKill.textContent = 10;
-    }
+// funktion för att öka antalet plankor
+function ökaAntalPlankor() {
+  antalPlankor += 1;
+  const plankorElement = document.querySelector(".wood-resources");
+  plankorElement.textContent = antalPlankor;
+}
 
-    if (zombieClickCounter === reqClicks) {
-      antalPlankor += 1;
+// funktion för att hantera klick på zombien
+function hanteraZombieKlick() {
+  zombieClickCounter++;
+  clicksTKill.textContent -= 1;
 
-      zombieClickCounter = 0;
+  if (clicksTKill.textContent == 0) {
+    const zombieTyp = Math.floor(Math.random() * 3) + 1; //Slumpa en zombie
+    const slumpadeResurser = genereraSlumpadeResurser(zombieTyp); //Slumpa resurser
 
-      const plankorElement = document.querySelector(".wood-resources");
-      plankorElement.textContent = antalPlankor;
-    }
-  });
-});
+    antalPlankor += slumpadeResurser.plankor;
+    antalPengar += slumpadeResurser.pengar;
+    antalMaterial += slumpadeResurser.material;
+    antalVapendelar += slumpadeResurser.vapendelar;
+
+    const plankorElement = document.querySelector(".wood-resources");
+    const pengarElement = document.querySelector(".money-resources");
+    const materialElement = document.querySelector(".material-resources");
+    const vapendelarElement = document.querySelector(".weapon-parts-resources");
+
+    plankorElement.textContent = antalPlankor;
+    pengarElement.textContent = antalPengar;
+    materialElement.textContent = antalMaterial;
+    vapendelarElement.textContent = antalVapendelar;
+
+    clicksTKill.textContent = 10;
+  }
+
+  if (zombieClickCounter === reqClicks) {
+    ökaAntalPlankor();
+    zombieClickCounter = 0;
+  }
+}
+
+// funktion som körs när sidan har laddats helt
+function init() {
+  zombieImage.addEventListener("click", hanteraZombieKlick);
+}
+
+// lyssna på "load"-händelsen för att köra init-funktionen
+window.addEventListener("load", init);
