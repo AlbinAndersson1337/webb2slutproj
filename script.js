59c6ae673938a98d20ebc0c261b5b288f5320dd5
AlbinAndersson1337 <118989634+AlbinAndersson1337@users.noreply.github.com>
2023-12-11 20:42:00 +0100

    ändringar som fan S/o kingen

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const clicksToKillElement = document.querySelector(".clicks-to-kill");
 const plankorElement = document.querySelector(".wood-resources");
 const pengarElement = document.querySelector(".money-resources");
 const materialElement = document.querySelector(".material-resources");
+
+const resourceReferences = [plankorElement, pengarElement, materialElement];
+
 const vapendelarElement = document.querySelector(".weapon-parts");
 const stats = document.querySelector(".stats");
 
@@ -20,9 +23,49 @@ const baseUpgradeBtn = document.querySelector(".upgrade-base");
 const weaponUpgradeBtn = document.querySelector(".upgrade-weapon");
 
 let resourcesInterval = null;
+let testInterval = null;
 
 const baseUpgradeSection = document.querySelector(".baseUpgrades");
 const weaponUpgradeSection = document.querySelector(".weaponUpgrades");
+
+const allInfo = {
+  resurser: {
+    plankor: {
+      value: 0,
+      inc: 0,
+      ref: plankorElement,
+    },
+    pengar: {
+      value: 0,
+      inc: 0,
+      ref: pengarElement,
+    },
+    material: {
+      value: 0,
+      inc: 0,
+      ref: materialElement,
+    },
+    vapendelar: {
+      value: 0,
+      inc: 0,
+      ref: vapendelarElement,
+    },
+  },
+};
+
+const clicksToKillobj = {
+  zombies: {
+    zombie1: {
+      clicksTKill: 10,
+    },
+    zombie2: {
+      clicksTKill: 30,
+    },
+    zombie3: {
+      clicksTKill: 50,
+    },
+  },
+};
 
 //Uppgradering visuellt
 weaponUpgradeSection.style.display = "none";
@@ -36,31 +79,6 @@ weaponUpgradeBtn.addEventListener("click", function () {
   baseUpgradeSection.style.display = "none";
   weaponUpgradeSection.style.display = "block";
 });
-// ------------------------------
-/*
-const baseUpgrade1 = document.querySelector(".baseUpgrade1");
-const baseUpgrade2 = document.querySelector(".baseUpgrade2");
-let plankGainInterval;
-let baseUpgradeCount = 0;
-
-baseUpgrade1.addEventListener("click", function () {
-  const upgradeCost = calculateUpgradeCost(baseUpgradeCount);
-
-  if (antalPlankor >= upgradeCost) {
-    const bUpg1Text = document.querySelector(".bUpg1");
-
-    antalPlankor -= 6;
-    plankorElement.textContent = antalPlankor;
-    bUpg1Text.textContent = upgradeCost;
-
-    plankGainInterval = setInterval(function () {
-      antalPlankor += 1;
-      plankorElement.textContent = antalPlankor;
-    }, 1000);
-
-    baseUpgradeCount += 1;
-  }
-});*/
 
 function handleUpgrade(upgradeElement, resources, cost, rate) {
   let count = 0;
@@ -68,68 +86,72 @@ function handleUpgrade(upgradeElement, resources, cost, rate) {
   upgradeElement.addEventListener("click", function () {
     const upgradeCost = calculateUpgradeCost(cost, count);
 
-    console.log("Clicked");
-    console.log("Base cost:", cost);
-    console.log("Count:", count);
-    console.log("Upgrade cost:", upgradeCost);
-
-    if (resourcesAreSufficient(resources, upgradeCost)) {
-      console.log("Sufficient resources");
-      spendResources(resources, upgradeCost);
-      startResourceGainInterval(resources, rate);
+    if (resourcesAreSufficient(upgradeCost)) {
+      spendResources(upgradeCost);
+      startResourceGainInterval(rate);
 
       count += 1;
       updateUpgradeCost(upgradeElement, cost, count);
+      displayResourceValues();
     } else {
-      console.log("Insufficient resources");
     }
   });
 }
 
 function calculateUpgradeCost(baseCost, count) {
-  console.log("Base cost:", baseCost);
-  console.log("Count:", count);
+  let ret = [];
+  for (let i = 0; i < baseCost.length; i++) {
+    ret.push(Math.ceil(baseCost[i] * Math.pow(1.05, count)));
+    console.log(ret);
+  }
+  return ret;
   return Math.ceil(baseCost * Math.pow(1.05, count));
-  console.log("Upgrade cost:", upgradeCost);
-  return upgradeCost;
 }
 
-function resourcesAreSufficient(resources, cost) {
-  console.log("Resources:", resources);
-  console.log("Cost:", cost);
-  return resources.every((resource, index) => resource >= cost[index]);
-}
-
-function spendResources(resources, cost) {
-  resources.forEach((resource, index) => (resources[index] -= cost[index]));
-
-  resources.forEach((resource, index) =>
-    updateResourceDisplay(index, resource)
+function resourcesAreSufficient(cost) {
+  return Object.keys(allInfo.resurser).every(
+    (r, i) => allInfo.resurser[r].value >= cost[i]
   );
 }
 
-function startResourceGainInterval(resources, rate) {
-  if (!resourcesInterval) {
-    resourcesInterval = setInterval(function () {
-      resources.forEach((resource, index) => {
-        resources[index] += rate[index];
-        updateResourceDisplay(index, resources[index]);
+function spendResources(cost) {
+  Object.keys(allInfo.resurser).forEach(
+    (r, i) => (allInfo.resurser[r].value -= cost[i])
+  );
+}
+
+function startResourceGainInterval(rate) {
+  // Update inc
+  Object.keys(allInfo.resurser).forEach(
+    (r, i) => (allInfo.resurser[r].inc += rate[i])
+  );
+  if (!testInterval) {
+    testInterval = setInterval(() => {
+      Object.keys(allInfo.resurser).forEach((r) => {
+        allInfo.resurser[r].value += allInfo.resurser[r].inc;
       });
+      // update Resource Display in similar manner.
+      displayResourceValues();
     }, 1000);
   }
 }
 
-function updateResourceDisplay(resourceType, value) {
-  // Update the display for the specified resource type
-  const element = document.querySelector(`.${resourceType}-resources`);
-  if (element) {
-    element.textContent = value;
+function displayResourceValues() {
+  Object.keys(allInfo.resurser).forEach((r) => {
+    allInfo.resurser[r].ref.textContent = allInfo.resurser[r].value;
+  });
+}
+
+function updateResourceDisplay(index, value) {
+  const resourceReference = resourceReferences[index];
+  if (resourceReference) {
+    resourceReference.textContent = value;
   }
 }
 
 function updateUpgradeCost(upgradeElement, baseCost, count) {
   // Update the display for the upgrade cost
-  const costElement = upgradeElement.querySelector(".upgrade-cost");
+  const costElement = upgradeElement.querySelector(".bUpg1");
   if (costElement) {
     const upgradeCost = calculateUpgradeCost(baseCost, count);
     costElement.textContent = upgradeCost;
@@ -142,14 +164,14 @@ let baseUpgrade2 = document.querySelector(".baseUpgrade2");
 handleUpgrade(
   baseUpgrade1,
   [antalPlankor, antalPengar, antalMaterial],
-  [6, 20, 10],
-  [1, 5, 3]
+  [6, 20, 10, 0], // New 0 for vapendelar
+  [1, 5, 3, 0] // New 0 for vapendelar
 );
 handleUpgrade(
   baseUpgrade2,
   [antalPlankor, antalPengar, antalMaterial],
-  [10, 30, 20],
-  [2, 10, 5]
+  [10, 30, 20, 0],
+  [2, 10, 5, 0]
 );
 
 let killCount = 0;
@@ -163,6 +185,7 @@ function genereraSlumpadeResurser(zombieTyp) {
         pengar: 100,
         material: Math.floor(Math.random() * 3) + 3,
         vapendelar: Math.floor(Math.random() * 3) + 1,
+        clicksTKill: 10,
       };
     case 2:
       return {
@@ -170,6 +193,7 @@ function genereraSlumpadeResurser(zombieTyp) {
         pengar: 300,
         material: Math.floor(Math.random() * 7) + 9,
         vapendelar: Math.floor(Math.random() * 4) + 3,
+        clicksTKill: 30,
       };
     case 3:
       return {
@@ -177,6 +201,7 @@ function genereraSlumpadeResurser(zombieTyp) {
         pengar: 500,
         material: Math.floor(Math.random() * 6) + 15,
         vapendelar: Math.floor(Math.random() * 3) + 9,
+        clicksTKill: 50,
       };
     default:
       return {
@@ -208,11 +233,19 @@ function hanteraZombieKlick() {
     antalPengar += slumpadeResurser.pengar;
     antalMaterial += slumpadeResurser.material;
     antalVapendelar += slumpadeResurser.vapendelar;
+    clicksTKill = slumpadeResurser.clicksTKill;
+
+    allInfo.resurser["plankor"].value += slumpadeResurser.plankor;
+    allInfo.resurser["pengar"].value += slumpadeResurser.pengar;
+    allInfo.resurser["material"].value += slumpadeResurser.material;
+    allInfo.resurser["vapendelar"].value += slumpadeResurser.vapendelar;
+    clicksToKillobj["zombies"].value += slumpadeResurser.clicksTKill;
 
     plankorElement.textContent = antalPlankor;
     pengarElement.textContent = antalPengar;
     materialElement.textContent = antalMaterial;
     vapendelarElement.textContent = antalVapendelar;
+    clicksTKill.textContent = clicksTKill;
 
     zombieClickCounter = 0;
 
@@ -246,6 +279,23 @@ toggleButton.addEventListener("click", () => {
 
 window.addEventListener("load", (event) => {
   backgroundMusic.play();
+});
+
+const zmbImg = document.querySelector(".left-section");
+let state = false;
+
+zmbImg.addEventListener("click", () => {
+  if (clicksTKill > 0) {
+    if (zombieTyp === 1) {
+      zombieImage.style.backgroundImage = "url('images/zombie1.jpg')";
+    } else if (zombieTyp === 2) {
+      zombieImage.style.backgroundImage = "url('images/zombie2.png')";
+    } else if (zombieTyp === 3) {
+      zombieImage.style.backgroundImage = "url('images/zombie3.png')";
+    } else {
+      zombieImage.style.backgroundImage = "url('images/zombie1.jpg')";
+    }
+  }
 });
 
 // Funktion som körs när sidan har laddats helt
